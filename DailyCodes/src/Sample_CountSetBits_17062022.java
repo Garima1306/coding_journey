a7c3807cc1510e383175b600eab24cf97143378e
Garima Gupta <guptagarima466@gmail.com>
2022-06-17 00:58:59 +0530

    Count Set bit using gfg live lec

--- DailyCodes/src/Sample_CountSetBits_17062022.java
+++ DailyCodes/src/Sample_CountSetBits_17062022.java
@@ -5,24 +5,35 @@ public class Sample_CountSetBits_17062022 {
     {
         //System.out.println("Hello");
 
+//        int num=5;
+//        int anscount=countSetBits(num);
+//        System.out.println("count: "+anscount);
         int num=5;
-        int anscount=countSetBits(num);
-        System.out.println("count: "+anscount);
+        int count=0;
+        while(num>0)
+        {
+            if((num&1)==1)
+            {
+                count++;
+            }
+            num=num>>1;
+        }
+        System.out.println(count);
 
     }
-    public static int countSetBits(int num)
-    {
-        if(num==0)
-        {
-            return -1;
-        }
-        if(num%2==1)
-        {
-            count++;
-        }
-        System.out.println(num%2);
-            countSetBits(num/2);
-
-            return count;
-    }
+//    public static int countSetBits(int num)
+//    {
+//        if(num==0)
+//        {
+//            return -1;
+//        }
+//        if(num%2==1)
+//        {
+//            count++;
+//        }
+//        System.out.println(num%2);
+//            countSetBits(num/2);
+//
+//            return count;
+//    }
 }
